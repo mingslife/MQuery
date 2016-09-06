af45b1f305e4c2dfbcbc4fa47bfbbb364ee32638
Ming <642203604@qq.com>
2016-09-06 21:07:33 +0800

    添加trim方法

--- js/mquery.js
+++ js/mquery.js
@@ -13,5 +13,26 @@
 			return document.getElementById(selector.substring(1));
 		}
 	};
+	/*MQuery.$temp = {
+		$namespace: {}
+	};
+	MQuery.controller = function(name, func) {
+		this.$temp.$namespace[name] = {
+			scope: {},
+			func: {}
+		};
+		var controller = func(this.$temp.$namespace[name].scope);
+		for (var property in controller) {
+			var temp = controller[prototype];
+			if (typeof temp === "function") {
+				this.$temp.$namespace[name].func[prototype] = ;
+			}
+		}
+		// this.$temp.$namespace[name].controller = controller;
+	};*/
+	MQuery.trim = function(string) {
+		if (typeof string !== "string") throw TypeError(string + " is not a string.");
+		return String.prototype.trim ? string.trim() : string.replace(/^\s+|\s+$/g, "");
+	};
 	window.m = window.MQuery = MQuery;
 })(window);
